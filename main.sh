1c6dd2c2dc3533208c1c2b02cc4ca08c1e6f9830
veryheavypickle <xavier.carroll01@gmail.com>
2022-09-14 03:04:52 +0200

    Main will create author file if the file doesn't exist - added colours

--- main.sh
+++ main.sh
@@ -3,17 +3,44 @@
 # the directory that stores the repos
 repos="repositories/"
 currentDir=$(pwd)
+authorFile="authors.txt"
+declare authors=()
+
+# Text colouring
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+WHITE='\033[0;36m'
+NC='\033[0m' # No Color
 
 
 # https://www.git-tower.com/learn/git/faq/change-author-name-email/
 # https://devhints.io/git-log-format - for git log formats and variables
 
 main () {
+    if test -f "$authorFile"; then
+        echo "$authorFile exists."
+    else
+        echo -e "${WHITE}$authorFile ${NC}doesn't exist, ${GREEN}will create a new${NC} one based on the repositories in ${WHITE}$repos. "
+        findAllAuthors
+    fi
+}
+
+findAllAuthors() {
     for repo in $repos*;
     do
         if [ -d "$repo" ]; then
             findAuthors $repo
         fi
+    done
+
+    # remove dups again
+    authors=$(echo $authors | sort -u)
+    
+    for commiter in "${authors[@]}";
+    do
+        # authordata^
+        # while replacing % with ' ' again
+        echo $commiter"^"| sed 's/%/ /g' >> $authorFile
     done
 }
 
@@ -24,13 +51,12 @@ findAuthors () {
     cd $1
     # get all author and commiter data, then replace any spaces with '%'
     # bc bash CANNOT handle spaces
-    commiters=($(git log --pretty="format:%aN,%cN,%aE,%cE" | sed 's/ /%/g'))
+    # then remove duplicates
+    local commiters=($(git log --pretty="format:%aN,%cN,%aE,%cE" | sed 's/ /%/g' | sort -u))
     cd $currentDir
-    for commiter in "${commiters[@]}";
-    do
-        echo $commiter
-    done
 
+    # merge two arrays together, global one with local one
+    authors=("${authors[@]}" "${commiters[@]}")
 }
 
 main
