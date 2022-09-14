2eec61429c9d04ceac7683a3f6f6a06c2c028afe
veryheavypickle <xavier.carroll01@gmail.com>
2022-09-14 04:06:16 +0200

    Changed ^ to ,

--- main.sh
+++ main.sh
@@ -12,10 +12,6 @@ GREEN='\033[0;32m'
 WHITE='\033[0;36m'
 NC='\033[0m' # No Color
 
-
-# https://www.git-tower.com/learn/git/faq/change-author-name-email/
-# https://devhints.io/git-log-format - for git log formats and variables
-
 main () {
     if test -f "$authorFile"; then
         echo -e "${WHITE}$authorFile${NC} exists."
@@ -41,12 +37,12 @@ findAllAuthors() {
     do
         # authordata^
         # while replacing % with ' ' again
-        echo $commiter"^"| sed 's/%/ /g' >> $authorFile
+        echo $commiter","| sed 's/%/ /g' >> $authorFile
     done
 
     echo -e "\n${WHITE}$authorFile${NC} created\nIn order to continue, copy the updated author data after the ${WHITE}^${NC}\n"
-    echo -e "${RED}Example 1${NC} changing author data\n${WHITE}pickle,pickle,pickle@gmail.com,pickle@gmail.com^${GREEN}yoda,yoda,yoda@gmail.com,yoda@gmail.com\n"
-    echo -e "${RED}Example 2${NC} data remains the same\n${WHITE}pickle,pickle,pickle@gmail.com,pickle@gmail.com^${NC}\n"
+    echo -e "${RED}Example 1${NC} changing author data\n${WHITE}pickle,pickle,pickle@gmail.com,pickle@gmail.com,${GREEN}yoda,yoda,yoda@gmail.com,yoda@gmail.com\n"
+    echo -e "${RED}Example 2${NC} data remains the same\n${WHITE}pickle,pickle,pickle@gmail.com,pickle@gmail.com,${NC}\n"
     echo -e "${NC}Run the script again once completed"
 }
 
@@ -58,6 +54,8 @@ findAuthors () {
     # get all author and commiter data, then replace any spaces with '%'
     # bc bash CANNOT handle spaces
     # then remove duplicates
+
+    # https://devhints.io/git-log-format - for git log formats and variables
     local commiters=($(git log --pretty="format:%aN,%cN,%aE,%cE" | sed 's/ /%/g' | sort -u))
     cd $currentDir
 
@@ -67,9 +65,10 @@ findAuthors () {
 
 checkAuthorFile () {
     while read line; do
-        # if line does not end with ^
-        if [ "${line: -1}" != "^" ]; then
+        # if line does not end with ,
+        if [ "${line: -1}" != "," ]; then
             checkAuthorLine "${line}"
+            rewriteGit "${line}"
         fi
     done < "${authorFile}"
 }
@@ -79,19 +78,56 @@ checkAuthorLine () {
     # line is expected to be in modified form - example
     # pickle,pickle,pickle@gmail.com,pickle@gmail.com^yoda,yoda,yoda@gmail.com,yoda@gmail.com
     # sed is to remove space
-    local numArrow=$(grep -o "^" <<<"$1" | wc -l | sed 's/ //g')
     local numComma=$(grep -o "," <<<"$1" | wc -l | sed 's/ //g')
 
     #echo $numComma $numArrow $1
-    if [ "$numArrow" != "1" ]; then
+    if [ "$numComma" != "7" ]; then
         echo -e "${RED}Error, line is incorrectly formatted\n${WHITE}$1${NC}"
         exit
     fi
-    if [ "$numComma" != "6" ]; then
-        echo -e "${RED}Error, line is incorrectly formatted\n${WHITE}$1"
-        exit
-    fi
     
+}
+
+rewriteGit () {
+    # Takes in one string of author data (validated)
+    # https://www.git-tower.com/learn/git/faq/change-author-name-email/
+    # This is very inefficient but it means I only have to deal with one
+    # author to correct at a time - and plus, who is running this everyday?
+
+    # iterate through each repo
+    for repo in $repos*;
+    do
+        if [ -d "$repo" ]; then
+            cd $repo
+
+            # magic
+            echo $1
+             
+            cd $currentDir
+        fi
+    done
+}
+
+gitFilter () {
+    # Takes in one string of author data (validated)
+
+
+    git filter-branch --env-filter '
+    WRONG_EMAIL='wrong@example.com'
+    NEW_NAME="New Name Value"
+    NEW_EMAIL="correct@example.com"
+
+    if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+    then
+        export GIT_COMMITTER_NAME="$NEW_NAME"
+        export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+    fi
+    if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+    then
+        export GIT_AUTHOR_NAME="$NEW_NAME"
+        export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+    fi
+    ' --tag-name-filter cat -- --branches --tags
 }
 
 main
